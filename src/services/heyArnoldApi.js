71a7f5bb7c9535c217cea0996b010a22b1de799d
‘Katie <ktschr11@gmail.com>
2022-01-10 17:43:00 -0800

    character list page rendering list of characters

--- src/services/heyArnoldApi.js
+++ src/services/heyArnoldApi.js
@@ -3,20 +3,53 @@ export const getCharacters = async (page = 1) => {
     const res = await fetch(
       `https://hey-arnold-api.herokuapp.com/api/v1/characters?perPage=5&page=${page}`
     );
-    const results = await res.json();
-    return results[0];
+    return await res.json();
+
+    // const list = results.map((char) => ({
+    //   id: char._id,
+    //   name: char.name,
+    //   image: char.image,
+    // }));
+    
+    // for(let char of list) {
+    //   const randomGif = await getRandomGif();
+    //   return {
+    //     id: char.id,
+    //     name: char.name,
+    //     image:char.image,
+    //     randomGif
+    //   }
+    // }
+    // console.log(list);
+    // return list;
+
   } catch(error) {
     console.error(error.message);
   }
 }
-export const getRandomGif = async () => {
+
+
+const getRandomGifList = async () => {
   try {
     const res = await fetch(
-      `https://hey-arnold-api.herokuapp.com/api/v1/gifs/random`
+      `https://hey-arnold-api.herokuapp.com/api/v1/gifs/random?count=5`
     );
-    const response = res[0].json();
+    const response = await res.json();
+    console.log(response);
     return response;
-  } catch(e) {
+  } catch(error) {
     console.error(error.message);
   }
 }
+// const getRandomGif = async () => {
+//   try {
+//     const res = await fetch(
+//       `https://hey-arnold-api.herokuapp.com/api/v1/gifs/random`
+//     );
+//     const response = await res.json();
+//     console.log(response[0].gifLink);
+//     return response[0].gifLink;
+//   } catch(error) {
+//     console.error(error.message);
+//   }
+// }
--- src/views/Character/CharacterList.jsx
+++ src/views/Character/CharacterList.jsx
@@ -1,10 +1,39 @@
-import { getCharacters, getRandomGif } from "../../services/heyArnoldApi.js"
+import { handleRequest } from "msw";
+import { useEffect, useState } from "react"
+import { getCharacters } from "../../services/heyArnoldApi.js"
 
 
 export default function CharacterList() {
+  const [loading, setLoading] = useState(true);
+  const [characterList, setCharacters] = useState([]);
+
+  console.log(characterList);
+
+  useEffect(async () => {
+    try {
+      const res = await getCharacters();
+      setCharacters(res);
+      setLoading(false);
+    } catch (error) {
+      throw new Error(error)
+    }
+  }, [])
+
   return (
     <div>
       <h1>this is the character list</h1>
+      {loading ? <p>loading up</p> : <p>all loaded</p>
+      }
+      <ul>
+      {characterList.map((char) => 
+        <li>
+          {char.name}
+          <img src={char.image} alt={char.name} />
+        </li>
+      )
+      }
+      </ul>
+      
     </div>
   )
 }
